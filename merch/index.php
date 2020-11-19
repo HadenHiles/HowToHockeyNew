d0c833ced5ee0fc2fc205d7b18b359a91912ee26
HadenHiles <haden@howtohockey.com>
2020-11-19 16:25:08 -0500

    Update link for toques merch

--- merch/index.php
+++ merch/index.php
@@ -161,7 +161,7 @@ wp_enqueue_script( 'merch', '/merch/js/main.js');
 
                                 <div class="banner_caption style_minimal">
 
-                                    <a class="caption_text merch-link" id="toques" href="#" target="_blank">
+                                    <a class="caption_text merch-link" id="toques" href="/toques" target="_blank">
 
                                         <p class="banner_text">Toques</p>
 
