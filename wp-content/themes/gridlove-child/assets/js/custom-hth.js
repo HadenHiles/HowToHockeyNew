000dc178b9d2a4a0a03d51ebc1688093d8e4c664
HadenHiles <haden@howtohockey.com>
2021-09-21 11:53:13 -0400

    Hide disqus ads in comments section

--- wp-content/themes/gridlove-child/assets/js/custom-hth.js
+++ wp-content/themes/gridlove-child/assets/js/custom-hth.js
@@ -1,7 +1,22 @@
-(function($) {
+(function ($) {
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('table').wrap('<div class="table-wrap"></div>');
+
+    waitForElement("#disqus_thread", () => {
+      $('#disqus_thread iframe:first-child').css({ 'display': 'none' });
+    });
+
   });
+
+  function waitForElement(elementPath, callBack) {
+    window.setTimeout(() => {
+      if ($(elementPath).length) {
+        callBack(elementPath, $(elementPath));
+      } else {
+        waitForElement(elementPath, callBack);
+      }
+    }, 500)
+  }
 
 })(jQuery);
